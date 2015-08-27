41e5e48a4de1e2d17fe7408ea37f4312316b4483
willovent <william.klein@viacesi.fr>
2015-08-27 12:16:52 +0200

    cleanup

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
-
 function JQueryRequireFunc() {
 
-	//Make every elements roll on the screen
-    window.rollOverEverything = function(){
-    	$('body>*>*>*>* *').css({transition : "all 5s", transform : "rotate(1080deg) scale(0.8)"});
-    	setTimeout(function(){
-    		$('body>*>*>*>* *').css({transform : ""});
-    	}, 5*1000);
-    	setTimeout(function(){
-    		$('body>*>*>*>* *').css({transition : ""});
-    	}, 10*1000);
+    //Make every elements roll on the screen
+    window.rollOverEverything = function() {
+        $('body>*>*>*>* *').css({
+            transition: "all 5s",
+            transform: "rotate(1080deg) scale(0.8)"
+        });
+        setTimeout(function() {
+            $('body>*>*>*>* *').css({
+                transform: ""
+            });
+        }, 5 * 1000);
+        setTimeout(function() {
+            $('body>*>*>*>* *').css({
+                transition: ""
+            });
+        }, 10 * 1000);
     };
 
-    window.beatingHearth = function(rep,freq){
-    	freq = freq || 700;
-    	rep = rep || 10;
-    	$('body>*>*>*>* *').css({transition : "all "+freq+"ms"});
-    	var up = true;
-    	var beat = function(){
-    		if(rep<0) {
-    			$('body>*>*>*>* *').css({transform : "", transition : ""});
-    			return;
-    		}
-    		if(up)
-				$('body>*>*>*>* *').css({transform : "scale(1.001)"});
-			else
-				$('body>*>*>*>* *').css({transform : "scale(.999)"});
-			up = !up;
-			rep--;
-			setTimeout(beat,freq);
-    	};
-    	beat();
+    window.beatingHearth = function(rep, freq) {
+        freq = freq || 700;
+        rep = rep || 10;
+        $('body>*>*>*>* *').css({
+            transition: "all " + freq + "ms"
+        });
+        var up = true;
+        var beat = function() {
+            if (rep < 0) {
+                $('body>*>*>*>* *').css({
+                    transform: "",
+                    transition: ""
+                });
+                return;
+            }
+            if (up)
+                $('body>*>*>*>* *').css({
+                    transform: "scale(1.001)"
+                });
+            else
+                $('body>*>*>*>* *').css({
+                    transform: "scale(.999)"
+                });
+            up = !up;
+            rep--;
+            setTimeout(beat, freq);
+        };
+        beat();
     };
-
 }
+
 //include JQuery cdn if needed
-if(typeof jQuery=='undefined') {
+if (typeof jQuery == 'undefined') {
     var headTag = document.getElementsByTagName("head")[0];
     var jqTag = document.createElement('script');
     jqTag.type = 'text/javascript';
@@ -43,5 +58,5 @@ if(typeof jQuery=='undefined') {
     jqTag.onload = JQueryRequireFunc;
     headTag.appendChild(jqTag);
 } else {
-     JQueryRequireFunc();
+    JQueryRequireFunc();
 }
