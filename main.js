a6edc2e39fada520eb0c2e8a2e0f443959c4ecde
willovent <william.klein@viacesi.fr>
2015-08-27 12:14:17 +0200

    func 2

--- main.js
+++ main.js
@@ -10,7 +10,28 @@ function JQueryRequireFunc() {
     	setTimeout(function(){
     		$('body>*>*>*>* *').css({transition : ""});
     	}, 10*1000);
-    }
+    };
+
+    window.beatingHearth = function(rep,freq){
+    	freq = freq || 700;
+    	rep = rep || 10;
+    	$('body>*>*>*>* *').css({transition : "all "+freq+"ms"});
+    	var up = true;
+    	var beat = function(){
+    		if(rep<0) {
+    			$('body>*>*>*>* *').css({transform : "", transition : ""});
+    			return;
+    		}
+    		if(up)
+				$('body>*>*>*>* *').css({transform : "scale(1.001)"});
+			else
+				$('body>*>*>*>* *').css({transform : "scale(.999)"});
+			up = !up;
+			rep--;
+			setTimeout(beat,freq);
+    	};
+    	beat();
+    };
 
 }
 //include JQuery cdn if needed
